cdf0b8690751d7e644cf2f40fca5a324b4192d63
Madison Saylor <maddie0264@gmail.com>
2024-04-05 21:51:56 -0400

    authentication actions implemented

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,9 @@ const ROOT_URL = 'http://localhost:9090/api';
 export const ActionTypes = {
   FETCH_POSTS: 'FETCH_POSTS',
   FETCH_POST: 'FETCH_POST',
+  AUTH_USER: 'AUTH_USER',
+  DEAUTH_USER: 'DEAUTH_USER',
+  AUTH_ERROR: 'AUTH_ERROR',
 };
 
 export function fetchGames() {
@@ -58,7 +61,7 @@ export function addNewGame(title, navigate) {
       title, content: '', coverUrl: '', tags: '',
     };
 
-    axios.post(`${ROOT_URL}/posts`, fields)
+    axios.post(`${ROOT_URL}/posts`, fields, { headers: { authorization: localStorage.getItem('token') } })
       .then((response) => {
         // once we are done fetching we can dispatch a redux action with the response data
         navigate('/');
@@ -78,7 +81,7 @@ export function updateGame(id, navigate, newTitle) {
   return (dispatch) => {
     const fields = { title: newTitle };
     // update the game
-    axios.put(`${ROOT_URL}/posts/${id}`, fields)
+    axios.put(`${ROOT_URL}/posts/${id}`, fields, { headers: { authorization: localStorage.getItem('token') } })
       .then((response) => {
         dispatch({ type: ActionTypes.FETCH_POST, payload: response.data });
       })
@@ -95,7 +98,7 @@ export function updateGame(id, navigate, newTitle) {
 // delete an individual game when clicking the delete button
 export function deleteGame(id, navigate) {
   return (dispatch) => {
-    axios.delete(`${ROOT_URL}/posts/${id}`)
+    axios.delete(`${ROOT_URL}/posts/${id}`, { headers: { authorization: localStorage.getItem('token') } })
       .then((response) => {
         // navigate to the games page
         navigate('/');
@@ -107,5 +110,80 @@ export function deleteGame(id, navigate) {
         // dispatch({ type: ActionTypes.ERROR_SET, error });
         // might you also want an ERROR_CLEAR action?
       });
+  };
+}
+
+// trigger to deauth if there is error
+// can also use in your error reducer if you have one to display an error message
+export function authError(error) {
+  return {
+    type: ActionTypes.AUTH_ERROR,
+    message: error,
+  };
+}
+
+export function signinUser({ email, password }, navigate) {
+  // takes in an object with email and password (minimal user object)
+  // returns a thunk method that takes dispatch as an argument
+  return (dispatch) => {
+    const fields = {
+      email, password,
+    };
+    // does an axios.post on the /signin endpoint and passes in { email, password}
+    axios.post(`${ROOT_URL}/signin`, fields)
+      .then((response) => {
+        // on success does:
+        dispatch({ type: ActionTypes.AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+
+        // navigate to the homepage
+        navigate('/');
+      })
+      .catch((error) => {
+        // on error should
+        dispatch(authError(`Sign In Failed: ${error.response.data}`));
+        // dispatch an error, use it in a separate error reducer. this is the beauty of redux.
+        // have an error component somewhere show it
+        // dispatch({ type: ActionTypes.ERROR_SET, error });
+        // add an ERROR_CLEAR action?
+      });
+  };
+}
+
+export function signupUser({ email, password }, navigate) {
+  // takes in an object with email and password (minimal user object)
+  // returns a thunk method that takes dispatch as an argument
+  return (dispatch) => {
+    const fields = {
+      email, password,
+    };
+    // does an axios.post on the /signin endpoint and passes in { email, password}
+    axios.post(`${ROOT_URL}/signup`, fields)
+      .then((response) => {
+        // on success does:
+        dispatch({ type: ActionTypes.AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+
+        // navigate to the homepage
+        navigate('/');
+      })
+      .catch((error) => {
+        // on error should
+        dispatch(authError(`Sign Up Failed: ${error.response.data}`));
+        // dispatch an error, use it in a separate error reducer. this is the beauty of redux.
+        // have an error component somewhere show it
+        // dispatch({ type: ActionTypes.ERROR_SET, error });
+        // add an ERROR_CLEAR action?
+      });
+  };
+}
+
+// deletes token from localstorage
+// and deauths
+export function signoutUser(navigate) {
+  return (dispatch) => {
+    localStorage.removeItem('token');
+    dispatch({ type: ActionTypes.DEAUTH_USER });
+    navigate('/');
   };
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './style.scss';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-
+import { ActionTypes } from './actions';
 import rootReducer from './reducers';
 
 import App from './components/app';
@@ -12,6 +12,12 @@ import App from './components/app';
 const store = configureStore({
   reducer: rootReducer,
 });
+
+// set state as authenticated if a token was previously saved + available
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({ type: ActionTypes.AUTH_USER });
+}
 
 const root = createRoot(document.getElementById('main'));
 root.render(
