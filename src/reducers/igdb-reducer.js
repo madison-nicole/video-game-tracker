8ee2eebbaa54a23647e3a0257b2f181ec750b181
jeremy-washam <Jeremy.D.Washam.19@dartmouth.edu>
2024-04-23 20:17:28 -0700

    add top games action

--- src/reducers/igdb-reducer.js
+++ src/reducers/igdb-reducer.js
@@ -2,12 +2,15 @@ import { ActionTypes } from '../actions';
 
 const initialState = {
   results: [],
+  topGames: [],
 };
 
 const IGDBReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ActionTypes.IGDB_SEARCH:
       return { ...state, results: action.payload };
+    case ActionTypes.IGDB_TOP_GAMES:
+      return { ...state, topGames: action.payload };
     default:
       return state;
   }
