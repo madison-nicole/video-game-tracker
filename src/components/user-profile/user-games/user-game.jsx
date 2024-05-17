3cb7e55be658c6f295e281b42a591e9bee83b65f
Madison Saylor <maddie0264@gmail.com>
2024-05-17 13:27:05 -0400

    user profile games gray and blur by 1.5px on hover for edit and delete button visibility

--- src/components/user-profile/user-games/user-game.jsx
+++ src/components/user-profile/user-games/user-game.jsx
@@ -3,22 +3,25 @@ import { Image, CardBody, Card } from '@chakra-ui/react';
 
 function UserGame({ coverUrl, selectGame }) {
   return (
-    <Card
-      alignItems="center"
-      cursor="pointer"
-      justifyContent="center"
-      maxW="fit-content"
-      onClick={selectGame}
-    >
-      <CardBody padding="4px">
-        <Image
-          alt="Game cover"
-          borderRadius="lg"
-          maxH="200px"
-          src={coverUrl}
-        />
-      </CardBody>
-    </Card>
+    <div className="gray-on-hover">
+      <Card
+        alignItems="center"
+        cursor="pointer"
+        justifyContent="center"
+        maxW="fit-content"
+        onClick={selectGame}
+      >
+        <CardBody padding="4px">
+          <Image
+            alt="Game cover"
+            borderRadius="lg"
+            maxH="200px"
+            src={coverUrl}
+            transition="opacity 0.3s"
+          />
+        </CardBody>
+      </Card>
+    </div>
   );
 }
 
